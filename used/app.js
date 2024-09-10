bc4143311922d63761bcc79176332a24de124065
tantasin <118919223+tantasin@users.noreply.github.com>
2024-09-11 04:40:41 +0700

    Khangdum
    fish

---
+++ used/app.js
@@ -0,0 +1,5 @@
+function changeImage() {
+    if (document.getElementById('mainImage').src == 'o.png'){
+    document.getElementById('mainImage').src = 'oo.png';
+    } else document.getElementById('mainImage').src == 'o.png';
+}
